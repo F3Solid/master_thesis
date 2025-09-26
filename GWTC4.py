5512dba2bec03776a6aa202a0cf5b123e0aea911
Federico Leto di Priolo <federico.ldp@gmail.com>
2025-09-26 19:18:21 +0100

    Implemented spin distribution hyperparameters sampler
    Also done some minor cleaning to the code

--- GWTC4.py
+++ GWTC4.py
@@ -1,13 +1,23 @@
 import numpy as np
-import scipy
+from scipy.stats import rv_histogram
 from popsummary.popresult import PopulationResult
 
+# https://zenodo.org/records/16911563
 filename = "data/GWTC4/BBHSpin_MagTruncnormIidTiltIsotropicTruncnormNid.h5"
 result = PopulationResult(fname=filename)
 
-print(result.get_metadata('hyperparameters'))
-print(result.get_metadata('model_names'))
+# print(result.get_metadata('hyperparameters'))
 
-# print(help(result))
+# See Eq. B26 and B27 and Fig. 17 here: https://ui.adsabs.harvard.edu/abs/2025arXiv250818083T/abstract
+hyperparameters=['mu_chi', 'sigma_chi', 'mu_spin', 'sigma_spin', 'xi_spin']
+hyperposterior_samples = np.array(result.get_hyperparameter_samples(hyperparameters=hyperparameters)).T
 
-hyperposterior_samples = np.array(result.get_hyperparameter_samples())
+hyperposteriors = dict.fromkeys(hyperparameters)
+hypersamplers = dict.fromkeys(hyperparameters)
+for key, vals in zip(hyperparameters, hyperposterior_samples):
+    hyperposteriors[key] = vals
+    hypersamplers[key] = rv_histogram(np.histogram(vals, bins='auto', density=False), density=False)
+
+# This function samples from the posterior distribution of the specified parameter
+def hypersampler(par, size=1):
+    return hypersamplers[par].rvs(size=size)
