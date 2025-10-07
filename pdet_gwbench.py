031be87f89f689d9993cc258e2bb6096ca29944e
Federico Leto di Priolo <federico.ldp@gmail.com>
2025-10-07 17:59:16 +0100

    New test set and new methods to handle pdet grids
    Added methods to merge different pdet matrixes

--- pdet_gwbench.py
+++ pdet_gwbench.py
@@ -1,65 +1,120 @@
 import numpy as np
 from comp_pdet import pdet
-from tqdm.auto import tqdm
 from joblib import Parallel, delayed, cpu_count
+from tqdm.auto import tqdm
 import logging
 
-z_max_m_min = ((0.1, 1), (1, 30))
-z_min_m_max = ((6, 10),)
+# Initializes meshgrid, meshcoord and pdet_for_interpolant from fresh m1, m2 and z grids
+# Assumes that pdet is symmetric for mass swapping: for every (i, j, k) with i!=j, (j, i, k) is not included
+# grids=(m1grid, m2grid, zgrid)
+# z_max_m_min, z_min_m_max = ((z1, m1), (z2, m2), ...) are tuples defining combinations of redshifts and masses
+# giving respectively pdet=1 and pdet=0. For example, for z_max_m_min, anything with lower redshift and higher minumum mass will have pdet=1;
+# for z_min_m_max, anything with higher redshift and lower maximum mass will have pdet=0
+# If shuffle=True meshcoord and meshgrid will be shuffled before being returned
+# If pdet_for_interpolant is given then its NaN values will be initialized if possible
+def initialize_pdet_grids(grids, z_max_m_min, z_min_m_max, pdet_for_interpolant=None, shuffle=True):
+    def fill_pdet_matrix(grids, pdet_for_interpolant):
+        # Numpy meshgrid magic for advanced indexing
+        M1, M2, Z = np.meshgrid(*grids, indexing='ij')
+        IJK = np.meshgrid(*[np.arange(len(grid)) for grid in grids], indexing='ij')
+        for z_max, m_min in z_max_m_min:
+            mass_mask = (M1 >= m_min) & (M2 >= m_min)
+            z_mask = Z <= z_max
+            nan_mask = np.isnan(pdet_for_interpolant)
+            mask = mass_mask & z_mask & nan_mask
+            pdet_for_interpolant[*[indx[mask] for indx in IJK]] = 1
+        for z_min, m_max in z_min_m_max:
+            mass_mask = (M1 <= m_max) & (M2 <= m_max)
+            z_mask = Z >= z_min
+            nan_mask = np.isnan(pdet_for_interpolant)
+            mask = mass_mask & z_mask & nan_mask
+            pdet_for_interpolant[*[indx[mask] for indx in IJK]] = 0
 
-m1grid = np.concatenate((np.linspace(1, 5, 15, endpoint=False),
-                         np.linspace(5, 15, 30, endpoint=False),
-                         np.linspace(15, 30, 15, endpoint=False),
-                         np.linspace(30, 40, 30, endpoint=False),
-                         np.linspace(40, 50, 10)))
-m2grid = np.copy(m1grid)
-zgrid = np.concatenate((np.geomspace(1e-4, 1e-1, 200, endpoint=False),
-                        np.geomspace(1e-1, 1, 50, endpoint=False),
-                        np.geomspace(1, 10, 50)))
+    if pdet_for_interpolant is None:
+        pdet_for_interpolant = np.full([len(grid) for grid in grids], np.nan)
+    else:
+        shape_err_msg = f"pdet_for_interpolant has shape {pdet_for_interpolant.shape}. It must match the given grids with shape {tuple([len(grid) for grid in grids])}."
+        assert pdet_for_interpolant.shape == tuple([len(grid) for grid in grids]), shape_err_msg
+        pdet_for_interpolant = np.copy(pdet_for_interpolant)
+        
+    fill_pdet_matrix(grids, pdet_for_interpolant)
+    meshcoord, meshgrid = get_meshgrid_from_pdet_grids((*grids, pdet_for_interpolant))
 
-grids = (m1grid, m2grid, zgrid)
-pdet_for_interpolant = np.zeros([len(grid) for grid in grids])
+    # Shuffle the arrays to better ditribute load across processors
+    if shuffle:
+        p = np.random.permutation(len(meshcoord))
+        
+        meshcoord = meshcoord[p]
+        meshgrid = meshgrid[p]
 
-meshcoord = []
-meshgrid = []
+    print(f"pdet_for_interpolant number of points: {np.prod(pdet_for_interpolant.shape)}; meshcoord shape: {meshcoord.shape}")
 
-n_iter = 0
-for i, m1 in enumerate(m1grid):
-    for j, m2 in zip(range(i, len(m2grid)), m2grid[i:]):
-        for k, z in enumerate(zgrid):
-            n_iter += 1
+    return meshcoord, meshgrid, pdet_for_interpolant
 
-with tqdm(total=n_iter, desc="Populating pdet_for_interpolant and meshgrid") as pbar:
-    for i, m1 in enumerate(m1grid):
-        for j, m2 in zip(range(i, len(m2grid)), m2grid[i:]):
-            for k, z in enumerate(zgrid):
-                already_known=False
-                m12_min = m1 if m1 <= m2 else m2
-                for z_max, m_min in z_max_m_min:
-                    if z <= z_max and m12_min >= m_min:
-                        pdet_for_interpolant[i, j, k] = 1
-                        pdet_for_interpolant[j, i, k] = 1
-                        already_known=True
-                if not already_known:
-                    m12_max = m1 if m1 >= m2 else m2
-                    for z_min, m_max in z_min_m_max:
-                        if z >= z_min and m12_max <= m_max:
-                            already_known=True
-                if not already_known:
-                    meshcoord.append((i, j, k))
-                    meshgrid.append((m1 ,m2, z))
-                pbar.update(1)
+# Merges and updates two sets of (m1grid, m2grid, zgrid, pdet_for_interpolant) so that the returned set
+# contains all elements from the two sets only one time and old elements are overwritten with the new ones if requested
+# new_grids=(m1grid, m2grid, zgrid, pdet_for_interpolant)
+# if overwrite=False existing pdet won't be changed, otherwise they will be overwritten with new values if present
+def merge_pdet_grids(new_grids, existing_grid_file_path="", overwrite=False):
+    old_grids = np.load(existing_grid_file_path) # keys: m1grid, m2grid, zgrid, pdet_for_interpolant
+    assert len(old_grids) == len(new_grids) == 4, "The new_grids list must be of length 4, containing the new m1grid, m2grid, zgrid and pdet_for_interpolant"
+    new_grids = {key: val for key, val in zip(old_grids.keys(), new_grids)} # m1grid, m2grid, zgrid, pdet_for_interpolant
 
-meshcoord = np.array(meshcoord)
-meshgrid = np.array(meshgrid)
+    # Make the new grids
+    # keys: m1grid, m2grid, zgrid
+    unq_grids = {key: np.unique_all(np.concatenate((old_grids[key], new_grids[key])))
+                 for key in [*old_grids.keys()][:-1]}
 
-# Shuffle the arrays to better ditribute load across processors
-p = np.random.permutation(len(meshcoord))
+    # Make the new pdet matrix
+    pdet_for_interpolant = np.full([len(grid.values) for grid in unq_grids.values()], np.nan)
 
-meshcoord = meshcoord[p]
-meshgrid = meshgrid[p]
+    grids_to_process = [old_grids, new_grids]
+    for grids in grids_to_process:
+        # Get indexes of input grids elements inside of the new unq_grid
+        ijk = {key: np.searchsorted(unq_grids[key].values, grids[key])
+               for key in unq_grids.keys()}
+        # Numpy meshgrid magic for advanced indexing
+        IJK = np.meshgrid(*ijk.values(), indexing='ij')
 
-print(np.prod(pdet_for_interpolant.shape), meshcoord.shape)
+        # Fill pdet matrix according to the selected indexes
+        # Maybe all the unpackaging is not necessary, but it works. So DON'T TOUCH IT!
+        if overwrite:
+            pdet_for_interpolant[*IJK] = grids["pdet_for_interpolant"]
+        else:
+            nan_mask = np.isnan(pdet_for_interpolant[*IJK])
+            pdet_for_interpolant[*[indx[nan_mask] for indx in IJK]] = grids["pdet_for_interpolant"][nan_mask]
+
+    # Return only the merged grids
+    for key in unq_grids.keys():
+        unq_grids[key] = unq_grids[key].values
+    unq_grids["pdet_for_interpolant"] = pdet_for_interpolant
+    
+    return unq_grids
+
+# Get meshgrid and meshcoord for pdet computation for an already processed pdet_for_interpolant matrix
+# Assumes that pdet is symmetric for mass swapping: for every (i, j, k) with i!=j, (j, i, k) is not included
+# and the underlying m1, m2 and z grid
+# grids=(m1grid, m2grid, zgrid, pdet_for_interpolant)
+def get_meshgrid_from_pdet_grids(grids):
+    # Initalize variables
+    m1grid, m2grid, zgrid, pdet_for_interpolant = grids
+
+    # select NaN elements indexes
+    nan_coord = np.argwhere(np.isnan(pdet_for_interpolant))
+    # select all indexes with m1=m2 or m1 < m2
+    mesh_indexes = np.squeeze(np.argwhere((nan_coord[:, 0] == nan_coord[:, 1]) | (nan_coord[:, 0] < nan_coord[:, 1])))
+    # Get meshcoord
+    meshcoord = nan_coord[mesh_indexes]
+
+    # Numpy meshgrid magic for getting coordinates
+    M1_M2_Z = np.meshgrid(m1grid, m2grid, zgrid, indexing='ij')
+    # Get meshgrid using meshcoord as a mask on the numpy meshgrid output
+    # Transpose and unpackage due to shapes:
+    # meshcoord: (N, 3)
+    # MG: (len(m1grid), len(m2grid), len(zgrid))
+    meshgrid = np.array([MG[*meshcoord.T] for MG in M1_M2_Z]).T
+
+    return meshcoord, meshgrid
 
 def compute_pdet_for_interpolant(meshgrid, meshcoord=None, m1grid=None, m2grid=None, zgrid=None, pdet_for_interpolant=None,
                                  n_sample=1e3, n_jobs=-1, verbose=11,
@@ -193,17 +248,3 @@ def compute_pdet_for_interpolant(meshgrid, meshcoord=None, m1grid=None, m2grid=N
             logger.warning(f"There is a mismatch between the length of meshgrid ({len(meshgrid)}) and the completed tasks ({completed_tasks})")
         # If everything went smooth, return meshvalues
         return meshvalues
-
-meshvalues = compute_pdet_for_interpolant(meshgrid, meshcoord, m1grid, m2grid, zgrid, pdet_for_interpolant,
-                                          n_sample=5e2,
-                                          save_checkpoint=True,
-                                          save_step=-10,
-                                          checkpoint_file_path="data/pdet_computation_checkpoint.npz",
-                                          from_checkpoint=True)
-
-for ijk, val in zip(meshcoord, meshvalues):
-    i, j, k = ijk
-    pdet_for_interpolant[i, j, k] = val
-    pdet_for_interpolant[j, i, k] = val
-
-np.savez("data/pdet_nsamples_5e2_(attempt_4)", m1grid=m1grid, m2grid=m2grid, zgrid=zgrid, pdet_for_interpolant=pdet_for_interpolant)
