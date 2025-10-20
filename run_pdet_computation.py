9f5dc6993d78cb3daa2d05d324c87b9023c88f96
Federico <federico.ldp@gmail.com>
2025-10-20 17:05:41 +0200

    Fixed a minor bug

--- run_pdet_computation.py
+++ run_pdet_computation.py
@@ -20,7 +20,7 @@ _, _, pdet_for_interpolant = initialize_pdet_grids((m1grid, m2grid, zgrid), z_ma
 grids = merge_pdet_grids((m1grid, m2grid, zgrid, pdet_for_interpolant), old_file_path)
 meshcoord, meshgrid, pdet_for_interpolant = initialize_pdet_grids((grids["m1grid"], grids["m2grid"], grids["zgrid"]), z_max_m_min, z_min_m_max, pdet_for_interpolant=grids["pdet_for_interpolant"])
 
-meshvalues = compute_pdet_for_interpolant(meshgrid, meshcoord, grids["m1grid"], grids["m2grid"], grids["zgrid"], grids["pdet_for_interpolant"],
+meshvalues = compute_pdet_for_interpolant(meshgrid, meshcoord, grids["m1grid"], grids["m2grid"], grids["zgrid"], pdet_for_interpolant,
                                           n_sample=5e2,
                                           save_checkpoint=True,
                                           save_step=-10,
